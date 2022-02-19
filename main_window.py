49d5f698ff6fe9aa807ad7d5b9f4f65c11d24c9e
heyuncle <heyuncle543@gmail.com>
2022-02-19 10:09:57 -0500

    Cleanup and preferences menu

--- main_window.py
+++ main_window.py
@@ -2,7 +2,7 @@ import subprocess, sys, os, shutil
 import xml.etree.ElementTree as et
 
 from window import Ui_MainWindow
-from properties import Ui_Dialog
+from preferences import Ui_Dialog
 
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import *
@@ -23,23 +23,26 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         for i in sys.argv[1:]:
             self.open_mmtr(i)
 
-        self.setWindowIcon(QIcon('logo.ico'))
+        self.setWindowIcon(QIcon('etc/logo.ico'))
         self.setupUi(self)
         # self.setStyleSheet(PyQt5_stylesheets.load_stylesheet_pyqt5(style="style_Dark")) # qrainbowtheme option
-        # app.setStyleSheet(qdarktheme.load_stylesheet("dark")) # pyqtdarktheme option
+        # self.setStyleSheet(qdarktheme.load_stylesheet("dark")) # pyqtdarktheme option
         apply_stylesheet(self, theme='dark_blue.xml')  # qmaterial
         self.show()
         self.retranslateUi(MainWindow)
         
         self.newObjButton.clicked.connect(self.addItem)
         self.treeWidget.itemClicked.connect(self.changeProperties)
+        self.treeWidget.itemDoubleClicked.connect(self.treeWidget.editItem)
+        self.surrObjComboBox.currentTextChanged.connect(self.changeObjType)
+        self.colorPushButton.clicked.connect(self.changeColor)
 
         self.actionOpen.triggered.connect(self.open_from_dir)
         self.actionSave.triggered.connect(self.save_mmtr)
         self.actionSave_as.triggered.connect(self.save_mmtr_as)
-        self.actionDelete.triggered.connect(self.delItem)
-        self.colorPushButton.clicked.connect(self.changeColor)
-        self.actionMP.triggered.connect(lambda _: self.renderScene(True))
+        self.actionPreferences.triggered.connect(self.openPreferences)
+        self.actionDelete.triggered.connect(self.delItem) #TODO fix
+        self.actionMP.triggered.connect(lambda _: self.renderScene(True)) #TODO fix
 
         # Test project
         newScene = QTreeWidgetItem()
@@ -58,8 +61,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         newScene.addChild(newObject2)
         self.changeProperties()
 
-    def changeObjType():
-        pass
+    def changeObjType(self):
+        for i in self.treeWidget.selectedItems():
+            i.setText(2,self.surrObjComboBox.currentText())
 
     def changeColor(self):
         self.colorFrame.setStyleSheet("background-color: " + QtWidgets.QColorDialog.getColor().name())
@@ -88,6 +92,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         else:
             if len(list(dict.fromkeys([i.text(1) for i in self.treeWidget.selectedItems()]))) == 1:
                 if self.treeWidget.currentItem().text(1)=="Object":
+                        self.objTypeComboBox.show()
                         combinedProp = 3
                         for i in self.treeWidget.selectedItems():
                             combinedProp &= shared[i.text(2)]
@@ -103,12 +108,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         return name
 
     def open_mmtr(self, file):
-        display_names = {'NumberPlane': 'Coordinate Plane', 'ParametricFunction': 'Parametric Function',
-                         'Arc': 'Circle', 'RegularPolygon': 'Regular Polygon', 'SurroundingRectangle': 'Box',
-                         'BraceLabel': 'Brace', 'Tex': 'LaTeX', 'ShowCreation': 'Show Creation',
-                         'Write': 'Write Text', 'FadeIn': 'Fade In', 'FadeOut': 'Fade Out',
-                         'GrowFromCenter': 'Grow', 'Flash': 'Spark', 'Indicate': 'Highlight',
-                         'MoveAlongPath': 'Move Along Path', 'ReplacementTransform': 'Transform'}
+        # display_names = {'NumberPlane': 'Coordinate Plane', 'ParametricFunction': 'Parametric Function',
+        #                  'Arc': 'Circle', 'RegularPolygon': 'Regular Polygon', 'SurroundingRectangle': 'Box',
+        #                  'BraceLabel': 'Brace', 'Tex': 'LaTeX', 'ShowCreation': 'Show Creation',
+        #                  'Write': 'Write Text', 'FadeIn': 'Fade In', 'FadeOut': 'Fade Out',
+        #                  'GrowFromCenter': 'Grow', 'Flash': 'Spark', 'Indicate': 'Highlight',
+        #                  'MoveAlongPath': 'Move Along Path', 'ReplacementTransform': 'Transform'}
         self.objList.clear()
         self.animList.clear()
         with open(file, "r+") as f:
@@ -129,100 +134,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.file_path = file.replace("\\", "/")
         self.setWindowTitle("Manimator - " + self.file_path.split("/")[-1])
 
-    def openProperties(self, prop_type):
-        def submitProperties():
-            props = {}
-            for i in self.properties.propertyStack.currentWidget().findChildren(QLineEdit):
-                property_name = "_".join(i.objectName().split("_")[1:])
-                if property_name in ["obj", "object", "mobject"]:
-                    if i.text() not in self.objNames:
-                        return None
-                if i.text() == "":
-                    props[property_name] = i.placeholderText()
-                else:
-                    props[property_name] = i.text()
-            name = self.properties.nameEdit.text().replace(" ", "_")
-            if name != self.objSave[self.objList.currentRow()][2]:
-                name = self.testDuplicateName(name)
-            if prop_type == "obj":
-                props["show"] = self.properties.showCheckBox.isChecked()
-                color = self.properties.currentColor.styleSheet().split()[-1]
-                if color != "255);":
-                    props["color"] = color
-                self.objSave[self.objList.currentRow()] = [self.properties.propertyStack.currentWidget().objectName(), props,
-                                                      name]
-                self.objNames[self.objList.currentRow()] = name
-                self.objList.currentItem().setText(self.objList.currentItem().text().split("(")[0] + "(" + name + ")")
-            else:
-                self.animSave[self.animList.currentRow()] = [self.properties.propertyStack.currentWidget().objectName(),
-                                                        props]
-            self.propWindow.close()
-            print(self.objSave)
-            print(self.animSave)
-
-        def loadProperties(prop_type):
-            possibleProp = {"Coordinate Plane": 0, "Function": 1, "Parametric Function": 2, "Circle": 3, "Arrow": 4,
-                            "Polygon": 5, "Regular Polygon": 6, "Rectangle": 7, "Dot": 8, "Line": 9, "Matrix": 10,
-                            "Box": 11, "Underline": 12, "Brace": 13, "LaTeX": 14, "Text": 15, "Show Creation": 16,
-                            "Uncreate": 17, "Write Text": 18, "Fade In": 19, "Fade Out": 20, "Grow": 21, "Spark": 22,
-                            "Highlight": 23, "Move": 24, "Move Along Path": 25, "Transform": 26}
-            if prop_type == "obj":
-                for i in ["nameEdit", "nameLabel", "showCheckBox", "colorLabel", "colorSelect", "currentColor"]:
-                    exec("self.properties." + i + ".setEnabled(True)")
-                try:
-                    self.properties.showCheckBox.setChecked(self.objSave[self.objList.currentRow()][1]["show"])
-                except:
-                    pass
-                try:
-                    self.properties.currentColor.setStyleSheet(
-                        "background-color: " + self.objSave[self.objList.currentRow()][1]["color"])
-                except:
-                    pass
-                try:
-                    self.properties.nameEdit.setText(self.objSave[self.objList.currentRow()][2])
-                except:
-                    self.properties.nameEdit.setText(self.objList.currentItem().text().split("(")[1][:-1])
-                print(self.objSave)
-                print(self.animSave)
-                try:
-                    self.properties.propertyStack.setCurrentIndex(
-                        possibleProp[self.objList.currentItem().text().split('(')[0][:-1]])
-                except:
-                    self.propWindow.close()
-            else:
-                try:
-                    self.properties.propertyStack.setCurrentIndex(possibleProp[self.animList.currentItem().text()])
-                except:
-                    self.propWindow.close()
-            # Currently broken data validation
-            # for i in self.properties.propertyStack.currentWidget().findChildren(QLineEdit):
-            #     if i.placeholderText() in ["X Line Frequency","Y Line Frequency","X Coordinate","Y Coordinate","Start Angle","Angle","Radius","Stroke Width","Height","Width","Fill Opacity","Horizontal Movement","Vertical Movement"]:
-            #         i.setValidator(QDoubleValidator())
-            #     elif i.placeholderText()=="# of Sides":
-            #         i.setValidator(QIntValidator())
-            try:
-                for i in self.properties.propertyStack.currentWidget().findChildren(QLineEdit):
-                    eval(
-                        "i.setText(" + prop_type + "Save[self." + prop_type + "List.currentRow()][1]['_'.join(i.objectName().split('_')[1:])])")
-            except:
-                pass
-
-
-
-        self.propWindow = QtWidgets.QMainWindow()
-        self.properties = Ui_Dialog()
-        self.properties.setupUi(self.propWindow)
-        self.propWindow.setWindowIcon(QIcon('logo.ico'))
-        loadProperties(prop_type)
-        self.properties.confirm.clicked.connect(submitProperties)
-
-        self.propWindow.show()
+    def openPreferences(self):
+        self.prefWindow = QtWidgets.QDialog()
+        self.preferences = Ui_Dialog()
+        self.preferences.setupUi(self.prefWindow)
+        self.prefWindow.setStyleSheet(self.styleSheet())
+        self.prefWindow.show()
 
     def addItem(self):
         if self.treeWidget.currentItem().text(1) in ["Group","Scene"]:
             newItem = QTreeWidgetItem()
             newItem.setText(0,self.testDuplicateName("MyObject"))
             newItem.setText(1,"Object")
+            newItem.setText(2,"Rectangle")
             self.treeWidget.currentItem().addChild(newItem)
         else:
             msg = QMessageBox()
@@ -233,14 +157,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             msg.exec_()
 
     def delItem(self):
-        indices = [self.objList.row(i) for i in self.objList.selectedItems()]
-        for i in sorted(indices, reverse=True):
-            self.objList.takeItem(i)
-            self.objSave.pop(i)
-        indices = [self.animList.row(i) for i in self.animList.selectedItems()]
-        for i in sorted(indices, reverse=True):
-            self.animList.takeItem(i)
-            self.animSave.pop(i)
+        for i in self.treeWidget.selectedItems():
+            self.treeWidget.delete
+
 
     def convert_to_manim(self, type):
         with open("manim_" + type + ".py", "w+") as f:
@@ -314,33 +233,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             f.write("        self.wait()")
             f.close()
 
-    # def render_full(self):
-    #     if not self.save_mmtr():
-    #         return None
-    #     print(self.file_path)
-    #     os.chdir("/".join(self.file_path.split("/")[:-1]))
-    #     self.convert_to_manim("export")
-    #     subprocess.run("manim manim_export.py MyScene")
-    #     try:
-    #         os.replace("./media/videos/manim_export/1080p60/MyScene.mp4", "./Export.mp4")
-    #         shutil.rmtree('media')
-    #         shutil.rmtree('__pycache__')
-    #     except:
-    #         pass
-
-    # def render_preview(self):
-    #     if not self.save_mmtr():
-    #         return None
-    #     os.chdir("/".join(self.file_path.split("/")[:-1]))
-    #     self.convert_to_manim("preview")
-    #     subprocess.run("manim manim_preview.py MyScene -ql")
-    #     try:
-    #         os.replace("./media/videos/manim_preview/480p15/MyScene.mp4", "./Preview.mp4")
-    #         shutil.rmtree('media')
-    #         shutil.rmtree('__pycache__')
-    #     except:
-    #         pass
-
     def renderScene(self, full):
         if not self.save_mmtr():
             return None
@@ -381,37 +273,3 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.open_mmtr(QtWidgets.QFileDialog.getOpenFileName(filter="Manimator (*.mmtr)")[0])
         except:
             pass
-
-    def move_up(self):
-        obj_row = self.objList.currentRow()
-        anim_row = self.animList.currentRow()
-        if obj_row > 0:
-            self.objList.insertItem(obj_row - 1, self.objList.takeItem(obj_row))
-            tmp = self.objSave[obj_row - 1]
-            self.objSave[obj_row - 1] = self.objSave[obj_row]
-            self.objSave[obj_row] = tmp
-            self.objList.setCurrentRow(obj_row - 1)
-            self.objNames = [i[2] for i in self.objSave]
-        if anim_row > 0:
-            self.animList.insertItem(anim_row - 1, self.animList.takeItem(anim_row))
-            tmp = self.animSave[anim_row - 1]
-            self.animSave[anim_row - 1] = self.animSave[anim_row]
-            self.animSave[anim_row] = tmp
-            self.animList.setCurrentRow(anim_row - 1)
-
-    def move_down(self):
-        obj_row = self.objList.currentRow()
-        anim_row = self.animList.currentRow()
-        if -1 < obj_row < self.objList.count() - 1:
-            self.objList.insertItem(obj_row + 1, self.objList.takeItem(obj_row))
-            tmp = self.objSave[obj_row + 1]
-            self.objSave[obj_row + 1] = self.objSave[obj_row]
-            self.objSave[obj_row] = tmp
-            self.objList.setCurrentRow(obj_row + 1)
-            self.objNames = [i[2] for i in self.objSave]
-        if -1 < anim_row < self.animList.count() - 1:
-            self.animList.insertItem(anim_row + 1, self.animList.takeItem(anim_row))
-            tmp = self.animSave[anim_row + 1]
-            self.animSave[anim_row + 1] = self.animSave[anim_row]
-            self.animSave[anim_row] = tmp
-            self.animList.setCurrentRow(anim_row + 1)
