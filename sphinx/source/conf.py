4782cfb212045c4096ff9d5bda0af53675ad9afb
AidenBeau <abeauchesne@me.com>
2022-05-18 22:32:52 -0400

    add version AND release number

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -22,7 +22,8 @@ copyright = '2022, studiodb'
 author = 'studiodb'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+version = '1.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
